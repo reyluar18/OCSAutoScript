dd446f82725988f7c9bd536b0de5b96a1cc7ba0c
shigenojay <38946650+shigenojay@users.noreply.github.com>
2018-08-05 10:36:21 +0800

    Update OCSAutoScrptz.sh

--- OCSAutoScrptz.sh
+++ OCSAutoScrptz.sh
@@ -111,8 +111,8 @@ service nginx restart
 apt-get -y install zip unzip
 cd /home/vps/public_html
 wget http://github.com/shigeno143/OCSAutoScript/raw/master/OCSPanel.zip
-unzip OCS.zip
-rm -f OCS.zip
+unzip OCSPanel.zip
+rm -f OCSPanel.zip
 chown -R www-data:www-data /home/vps/public_html
 chmod -R g+rw /home/vps/public_html
 
